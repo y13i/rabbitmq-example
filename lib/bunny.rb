b8d82f81dafe7d96a767543864eddbbee93ce0f6
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2016-02-02 18:04:59 +0900

    modify bunny object initialization

--- lib/bunny.rb
+++ lib/bunny.rb
@@ -1,14 +1,13 @@
 require "bunny"
 
 def bunny
-  @bunny ||= Bunny.new([
-    "amqp://",
-    (ENV["RABBITMQ_USER"] || "guest"),
-    ":",
-    (ENV["RABBITMQ_PASSWORD"] || "guest"),
-    "@",
-    (ENV["RABBITMQ_HOST"] || "localhost"),
-    ":",
-    (ENV["RABBITMQ_PORT"] || "5672")
-  ].join)
+  @bunny ||= Bunny.new(
+    host:      (ENV["RABBITMQ_HOST"] || "localhost"),
+    port:      (ENV["RABBITMQ_PORT"] ? ENV["RABBITMQ_PORT"].to_i : 5672),
+    ssl:       false,
+    vhost:     "/",
+    user:      (ENV["RABBITMQ_USER"] || "guest"),
+    pass:      (ENV["RABBITMQ_PASSWORD"] || "guest"),
+    heartbeat: (ENV["RABBITMQ_HEARTBEAT"] ? ENV["RABBITMQ_HEARTBEAT"].to_i : :server),
+  )
 end
